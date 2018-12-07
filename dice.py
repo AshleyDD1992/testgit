b8b8d595d18a77e996001568d04f9e9b6405298a
AshleyDD1992 <44950498+AshleyDD1992@users.noreply.github.com>
2018-12-07 10:34:49 +0000

    makes a dice game

---
+++ dice.py
@@ -0,0 +1,15 @@
+import random
+
+min = 1
+max = 2 
+roll_agin = ("yes")
+
+while roll_agin == ("yes") or roll_agin ==("y"):
+    print ("rolling...")
+    print ("the numbers are...")
+    print (random.randint(min,max))
+    print (random.randint(min,max))
+
+    roll_agin = input("roll again?")
+
+
